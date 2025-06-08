f1c3dc218de98fdf1a2718e5525294f1e65b1ea8
UnderSet <hunglam12700@gmail.com>
2025-06-08 23:12:19 +0700

    chore:relocate crap more sensibly

--- lua/autorun/client/mwiihud.lua
+++ lua/autorun/client/mwiihud.lua
@@ -7,41 +7,6 @@ local scale = 1
 MWIIHUD.DebugReference = CreateClientConVar("MWIIHUD_Debug_DrawReference", 0, false, false, "debug: draw reference image, gives no shit about main toggle", 0, 2)
 MWIIHUD.Toggle = CreateClientConVar("MWIIHUD_Enable", 1, true, false, "Enables the HUD.", 0, 1)
 
-function MWIIHUD.NeededStuff()
-    -- runs on start and every time res is changed
-    scrw, scrh = ScrW(), ScrH()
-    scale = scrh / 1080
-
-    -- render target sizes MUST be power of 2 because dx9 or sum shit
-    MWIIHUD.WeaponIconRT = GetRenderTarget("MWIIWeaponIcon", 1024 * math.Round(scale), 512 * math.Round(scale))
-    MWIIHUD.WeaponIconRTMat = CreateMaterial( 
-        "MWIIWeaponIconMat","UnlitGeneric",
-        {
-            ["$basetexture"] = MWIIHUD.WeaponIconRT:GetName(),
-            ["$translucent"] = "1"
-        } 
-    )
-
-    surface.CreateFont("hl2wepicon", {
-        font = 'halflife2',
-        size = 160 * scale,
-        weight = 240
-    })
-    surface.CreateFont( "MWIIAmmoText", {
-        font = "Stratum2 BETA Medium", -- Use the font-name which is shown to you by your operating system Font Viewer.
-        size = 50 * scale,
-        weight = 60,
-        shadow = true,
-    } )
-    surface.CreateFont( "MWIIAmmoSubText", {
-        font = "Stratum2 BETA Medium", -- Use the font-name which is shown to you by your operating system Font Viewer.
-        size = 25 * scale,
-        weight = 60,
-        shadow = true,
-    } )
-end
-
-MWIIHUD.NeededStuff()
 
 MWIIHUD.HideCElements = {
     ["CHudHealth"] = true,
@@ -84,6 +49,42 @@ MWIIHUD.IconColorCorrectParam = {
 
 MWIIHUD.Assets = {}
 MWIIHUD.Assets.Reference = {{Material("mwii/reference/reference1.png", "noclamp smooth")}, {Material("mwii/reference/reference2.png", "noclamp smooth")}} -- dont blame me for le double
+
+function MWIIHUD.NeededStuff()
+    -- runs on start and every time res is changed
+    scrw, scrh = ScrW(), ScrH()
+    scale = scrh / 1080
+
+    -- render target sizes MUST be power of 2 because dx9 or sum shit
+    MWIIHUD.WeaponIconRT = GetRenderTarget("MWIIWeaponIcon", 1024 * math.Round(scale), 512 * math.Round(scale))
+    MWIIHUD.WeaponIconRTMat = CreateMaterial( 
+        "MWIIWeaponIconMat","UnlitGeneric",
+        {
+            ["$basetexture"] = MWIIHUD.WeaponIconRT:GetName(),
+            ["$translucent"] = "1"
+        } 
+    )
+
+    surface.CreateFont("hl2wepicon", {
+        font = 'halflife2',
+        size = 160 * scale,
+        weight = 240
+    })
+    surface.CreateFont( "MWIIAmmoText", {
+        font = "Stratum2 BETA Medium", -- Use the font-name which is shown to you by your operating system Font Viewer.
+        size = 50 * scale,
+        weight = 60,
+        shadow = true,
+    } )
+    surface.CreateFont( "MWIIAmmoSubText", {
+        font = "Stratum2 BETA Medium", -- Use the font-name which is shown to you by your operating system Font Viewer.
+        size = 25 * scale,
+        weight = 60,
+        shadow = true,
+    } )
+end
+
+MWIIHUD.NeededStuff()
 
 local function doNothing() end
 function MWIIHUD.DrawWeaponIconToRT(Weapon, x, y, width, h)
