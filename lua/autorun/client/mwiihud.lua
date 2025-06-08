f089fc422fa794dfe1048ac26493596ca3d82829
UnderSet <hunglam12700@gmail.com>
2025-06-08 18:13:40 +0700

    fix:make shit work

--- lua/autorun/client/mwiihud.lua
+++ lua/autorun/client/mwiihud.lua
@@ -1,4 +1,4 @@
-include("TFAKeys.lua")
+--include("TFAKeys.lua") -- include() fuck you why wont you work
 
 local MWIIHUD = {}
 local scrw, scrh = 0, 0
@@ -12,5 +12,19 @@ function MWIIHUD.NeededStuff()
     GetRenderTarget("MWIIWeaponIcon", 1024 * math.Round(scale), 512 * math.Round(scale))
 end
 
+MWIIHUD.NeededStuff()
+
 MWIIHUD.Assets = {}
-MWIIHUD.Assets.Reference = {Material("mwii/reference/reference1.png")}
+MWIIHUD.Assets.Reference = {{Material("mwii/reference/reference1.png", "noclamp smooth")}} -- dont blame me for le double
+
+function MWIIHUD.MainHook()
+    surface.SetMaterial(MWIIHUD.Assets.Reference[1][1])
+    surface.SetDrawColor(255,255,255,180)
+    surface.DrawTexturedRect(0,0,scrw,scrh)
+    surface.DrawRect(100,100,100,100)
+end
+
+hook.Add("HUDPaint", "MWIIHUDDraw", MWIIHUD.MainHook)
+
+print("MWII HUD loaded." .. SysTime())
+PrintTable(MWIIHUD.Assets.Reference)
